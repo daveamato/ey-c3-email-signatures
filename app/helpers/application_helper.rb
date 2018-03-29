5603349535570d6d2e7005963df844434918abb0
Peter Hollows <github@dojo7.com>
2018-03-29 13:01:44 +1100

    feedback_url: change to email

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,9 @@
 module ApplicationHelper
 
   def feedback_url
-    desc = "Please add your feedback here.\n\n--- debugging info ---\n#{Base64.encode64 params.to_json}"
-    query = { 'ticket[subject]' => 'Signature Feedback', 'ticket[description]' => desc }
-    uri = URI::HTTPS.build(host: 'c3products.zendesk.com', path: '/anonymous_requests/new', query: query.to_query)
-
-    uri.to_s
+    desc = "Please-add-your-feedback-here.\n\n\n\n\n---debugging-info---\n#{Base64.encode64 params.to_json}"
+    "mailto:support@eyc3.com?" + { subject: "Email Signature", body: desc }.to_query
   end
 
 end
+
